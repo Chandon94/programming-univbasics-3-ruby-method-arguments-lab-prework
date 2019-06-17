07338c676f7212184a644241b3cdbfd08917383c
Chandon Porter <cneilporter94@gmail.com>
2019-06-17 17:27:32 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -7,7 +7,7 @@ def introduction (name)
    puts "Hi, my name is #{name} and I am learning to program in #{language}."
  end
  
- def introduction_with_language_optional (name, language, defualt)
+ def introduction_with_language_optional (name, language = "Ruby")
 
    puts "Hi, my name is #{name} and I am learning to program in #{language}."
  end
